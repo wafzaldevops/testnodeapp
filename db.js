9b76c93cb1b3f2808e1c5e70a62edfc10f9f4b7e
wafzaldevops <167966414+wafzaldevops@users.noreply.github.com>
2024-06-11 01:55:02 +0500

    Update db.js

--- db.js
+++ db.js
@@ -1,4 +1,7 @@
 require('dotenv').config();
+console.log('Database host:', process.env.DB_HOST);
+console.log('Database user:', process.env.DB_USER);
+// ...other variables
 const mysql = require('mysql2');
 const connection = mysql.createConnection({
   host: process.env.DB_HOST,
